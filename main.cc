c147098154a042899c8583b16677843766189ef7
Kunal jain <kunaljainwin@gmail.com>
2025-05-03 11:31:28 +0530

    Initial Commit

---
+++ main.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+class Trie{
+    int arr[10]={0};
+    // bool bFlag;// not required but generally used for matching whole strings
+}
+
+int main(){
+    
+}
